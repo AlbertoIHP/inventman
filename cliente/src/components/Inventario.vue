c7bab263bd3ec59ed7aa997d10e9a8a46dc3b645
Alberto Ignacio <a.herrera07@ufromail.cl>
2017-10-27 04:59:54 -0300

    Se termina la priemra vista de inventario y se saca la tabla pics y se reemplaza por una url

--- cliente/src/components/Inventario.vue
+++ cliente/src/components/Inventario.vue
@@ -6,9 +6,44 @@
       </div>
     </div>
 
+    <md-layout md-gutter  v-for="(producto, index) in productosMostrar">
+      <md-layout   md-flex-offset="20">
+        <md-card class="tarjetaProducto">
+          <md-card-media>
+            <img src="http://www.bellezachile.cl/users/producto/493.jpg" class="imagenProducto">
+          </md-card-media>
 
+          <md-card-header>
+            <div class="md-title">{{producto.name}} </div>
+            <div class="md-subhead">{{producto.description}}</div>
+          </md-card-header>
 
-    <md-button class="md-fab md-fab-bottom-right" id="fab" @click="openDialog('dialog2')">
+          <md-card-actions>
+            <md-button  @click="openDialog('detallesProducto', producto)">Ver detalles</md-button>
+          </md-card-actions>
+        </md-card>
+      </md-layout>
+
+      <md-layout  >
+        <md-card class="tarjetaProducto">
+          <md-card-media>
+            <img src="http://www.bellezachile.cl/users/producto/493.jpg" class="imagenProducto">
+          </md-card-media>
+
+          <md-card-header>
+            <div class="md-title">{{producto.name}} </div>
+            <div class="md-subhead">{{producto.description}}</div>
+          </md-card-header>
+
+          <md-card-actions>
+            <md-button @click="openDialog('detallesProducto', producto)">Ver detalles</md-button>
+          </md-card-actions>
+        </md-card>
+      </md-layout>
+      <br>
+    </md-layout>
+
+    <md-button style="position: fixed" class="md-fab md-fab-bottom-right" id="fab" @click="openDialog('dialog2')">
       <md-icon>add</md-icon>
     </md-button>
 
@@ -25,12 +60,28 @@
       </md-dialog-content>
 
       <md-dialog-actions>
-        <md-button class="md-primary" @click="closeDialog('dialog2')">Cancel</md-button>
-        <md-button class="md-primary" @click="closeDialog('dialog2')">Create</md-button>
+        <md-button class="md-primary" @click="closeDialog('dialog2', '')">Cancel</md-button>
+        <md-button class="md-primary" @click="closeDialog('dialog2', '')">Create</md-button>
       </md-dialog-actions>
     </md-dialog>
 
+    <md-dialog md-open-from="#fab" md-close-to="#fab" ref="detallesProducto">
+      <md-dialog-title>Detalles producto {{detalle.name}}</md-dialog-title>
 
+      <md-dialog-content>
+        <form>
+          <md-input-container>
+            <label>Note</label>
+            <md-textarea></md-textarea>
+          </md-input-container>
+        </form>
+      </md-dialog-content>
+
+      <md-dialog-actions>
+        <md-button class="md-primary" @click="closeDialog('detallesProducto')">Cancel</md-button>
+        <md-button class="md-primary" @click="closeDialog('detallesProducto')">Create</md-button>
+      </md-dialog-actions>
+    </md-dialog>
 
 
   </section>
@@ -50,7 +101,8 @@
         productos: [],
         categorias: [],
         tipos: [],
-        productosMostrar: []
+        productosMostrar: [],
+        detalle: {name: ""}
       }
     },
     created() {
@@ -71,7 +123,8 @@
     },
     methods:
     {
-      openDialog(ref) {
+      openDialog(ref, producto) {
+        this.detalle = producto
         this.$refs[ref].open();
       },
       closeDialog(ref) {
@@ -141,5 +194,18 @@
 
 
 <style scoped>
+.imagenProducto
+{
+  width: 100%;
+   height: 105px;
+    max-width: 100%;
+    max-height: 150px;
+}
 
+.tarjetaProducto
+{
+
+  width: 60%;
+  margin-bottom: 30px;
+}
 </style>
